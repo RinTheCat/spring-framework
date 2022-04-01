b9543c43949d179e16a662321ee32ed6cf3bc3db
danilovae <liza@danilova.me>
2022-04-01 18:06:28 +0300

    кейс использования ReflectionTestUtils

--- hw4/src/main/java/ru/otus4/spring/config/IOProvider.java
+++ hw4/src/main/java/ru/otus4/spring/config/IOProvider.java
@@ -1,12 +1,11 @@
 package ru.otus4.spring.config;
 
-import java.io.InputStream;
+import java.io.BufferedReader;
 import java.io.PrintStream;
 import java.util.Locale;
 
 public interface IOProvider {
-    InputStream getInputStream();
+    BufferedReader getBufferedReader();
     PrintStream getPrintStream();
     Locale getCurrentLocale();
-    void setPrintStream(PrintStream printStream); // плохо
 }
