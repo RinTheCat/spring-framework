12c1fdac0c55c4cbf40568994f0cb60b83d5bc2d
danilovae <liza@danilova.me>
2022-06-29 19:27:53 +0300

    homework10

---
+++ hw10/src/main/java/ru/otus/ajax/dto/BookDto.java
@@ -0,0 +1,79 @@
+package ru.otus.ajax.dto;
+
+import ru.otus.ajax.domain.Book;
+import ru.otus.ajax.domain.Comment;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BookDto {
+    private long id;
+
+    private String title;
+
+    private List<String> comments;
+
+    private AuthorDto author;
+
+    private GenreDto genre;
+
+    public BookDto(long id, String title, AuthorDto author, GenreDto genre, List<String> comments) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.comments = comments;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<String> comments) {
+        this.comments = comments;
+    }
+
+    public AuthorDto getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDto author) {
+        this.author = author;
+    }
+
+    public GenreDto getGenre() {
+        return genre;
+    }
+
+    public void setGenre(GenreDto genre) {
+        this.genre = genre;
+    }
+
+    public Book toDomainObject() {
+        return new Book(0, this.title, null, null, null);
+    }
+    
+    public static BookDto toDto(Book book) {
+        return new BookDto(book.getId(),
+                book.getTitle(),
+                new AuthorDto(book.getAuthor().getId(), book.getAuthor().getFullName()),
+                new GenreDto(book.getGenre().getId(), book.getGenre().getName()),
+                book.getComments().stream().map(Comment::getText).collect(Collectors.toList()));
+    }
+}
---
+++ hw10/src/main/java/ru/otus/ajax/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package ru.otus.ajax.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException() {
+    }
+}
